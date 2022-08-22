503f2d1447d9c51bcf2f066c989725566cd89f29
Matthew Domi <70774927+matthewdomi@users.noreply.github.com>
2022-08-22 16:22:31 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-// Importation of functions in component
-import Header from "./Component/Header";
-import {Button1, Button2, Button3 } from "./Component/Button";
-import {Image} from "./Component/Image";
+// import Header from "./components/Header";
+// import Button from "./components/Button";
+// import { Image } from "./components/Image";
+// import Navbar from "./components/Navbar";
+// import Form from "./components/Form";
+// import TaskManager from "./components/TaskManager";
+import ProductList from "./components/ProductList";
 
-function App() {
-    return (
-    <div>
-     <Header />;
-     <Button1 />;
-     <Button2 />;
-     <Button3 />;
-     <Image />
-     </div>
-    );
+function App(){
+return (
+    <div> 
+        {/* <Navbar />
+        <Header />;
+        <Button label="Add to Cart" />
+        <Button label="Read more..." />
+        <Button label="Register" />
+        <Button label="Login" />
+        <Image /> */
+        // <Form />
+        // <TaskManager />
+        <ProductList />
+        }
+    </div>
+)
 }
+
 export default App;
