7f1fe236b3ffb9a2b039dffb92cbd47b0839f79b
Matthew Domi <70774927+matthewdomi@users.noreply.github.com>
2022-08-22 16:42:41 +0100

    Add files via upload

---
+++ src/components/New folder/useLocalStorage.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+function useLocalStorage(key, initialValue) {
+  const [data, setData] = useState(() => {
+    const result = localStorage.getItem(key);
+    if (!result) return initialValue;
+    return JSON.parse(result);
+  });
+
+  const setValue = (value) => {
+    // const newData = data.push(value);
+    const newData = [value, ...data];
+    setData(newData);
+  };
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(data));
+  }, [data, key]);
+
+  return { data, setValue };
+}
+
+export default useLocalStorage;
